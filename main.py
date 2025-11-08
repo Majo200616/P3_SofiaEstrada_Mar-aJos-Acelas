0a19d8dc89a3814bc7c7ac6be22d308021108f1a
Majo200616 <mariaj.acelas@udea.edu.co>
2025-11-08 15:56:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -56,22 +56,20 @@ volumen= loader.load()
 loader.mostrar_cortes()
 
 class EstudioImaginologico:
-    def __init__(self, patient_name, patient_id, study_date, study_time, modality, study_description):
-        self.patient_name = patient_name
-        self.patient_id = patient_id
+    def __init__(self, study_date, study_time, modality, study_description, series_time):
         self.study_date = study_date
         self.study_time = study_time
         self.modality = modality
         self.study_description = study_description
+        self.series_time = series_time
 
     def mostrar_info(self):
         print("Información del Estudio Imaginológico DICOM:")
-        print(f"Nombre del Paciente:     {self.patient_name}")
-        print(f"ID del Paciente:         {self.patient_id}")
         print(f"Fecha del Estudio:       {self.study_date}")
         print(f"Hora del Estudio:        {self.study_time}")
         print(f"Modalidad:               {self.modality}")
         print(f"Descripción del Estudio: {self.study_description}")
+        print(f"Hora de la Serie:        {self.series_time}")
 
     
 
